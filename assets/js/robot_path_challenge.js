6ea0dc7085f4282d50ad5b72232f0d19c5f9940a
nhtanvir93 <tanvir.eatl@gmail.com>
2026-06-12 15:46:20 +0600

    Feat: dynamic paths created in robot path challenge

---
+++ assets/js/robot_path_challenge.js
@@ -0,0 +1,59 @@
+const BLANK = 0;
+const ROBOT = 1;
+const STAR = 2;
+const WALL = 3;
+const DIAMOND = 4;
+
+const icons = {
+  [BLANK]: null,
+  [ROBOT]: "robot-icon",
+  [STAR]: "star-icon",
+  [WALL]: "wall-icon",
+  [DIAMOND]: "diamond-icon",
+};
+
+const levels = [
+  [
+    [WALL, WALL, WALL, WALL, WALL, WALL],
+    [WALL, BLANK, BLANK, BLANK, STAR, WALL],
+    [WALL, BLANK, WALL, WALL, BLANK, WALL],
+    [WALL, BLANK, WALL, WALL, DIAMOND, BLANK],
+    [ROBOT, BLANK, BLANK, WALL, WALL, DIAMOND],
+    [WALL, WALL, BLANK, BLANK, BLANK, BLANK],
+    [WALL, WALL, WALL, WALL, WALL, WALL],
+  ],
+];
+
+let currentLevel = 1;
+
+function buildPathGrid() {
+  const pathGrid = document.getElementById("path-grid");
+
+  const paths = [];
+  const level = levels[currentLevel - 1];
+
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 6; col++) {
+      const iconNo = level[row][col];
+
+      const path = document.createElement("div");
+      path.classList.add("path");
+
+      if (iconNo !== BLANK) {
+        const icon = document.createElement("div");
+        icon.classList.add(icons[iconNo], "icon");
+        path.append(icon);
+      }
+
+      path.setAttribute("data-row", row);
+      path.setAttribute("data-col", col);
+      path.setAttribute("data-icon-no", iconNo);
+
+      paths.push(path);
+    }
+  }
+
+  pathGrid.append(...paths);
+}
+
+buildPathGrid();
